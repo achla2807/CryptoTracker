b39c7c7ea80b3c6ac3cc1c0c8f3688ec1c5bb5dc
Achla Ajay <achla2807@gmail.com>
2025-03-19 21:39:11 +0530

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Achla Ajay on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
